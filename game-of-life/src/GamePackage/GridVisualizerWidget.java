7a7946e8642d14ffdd0d99427beb42c13d5af4ad
milenpatel <milenpatel@192.168.1.27>
2019-12-02 17:41:27 -0500

    UI Always has grid now, even for large dimensions

--- game-of-life/src/GamePackage/GridVisualizerWidget.java
+++ game-of-life/src/GamePackage/GridVisualizerWidget.java
@@ -33,32 +33,30 @@ public class GridVisualizerWidget extends JPanel implements MouseListener {
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-
 		Rectangle r = getBounds();
-		//this.setPreferredSize(new Dimension((int)(r.getWidth()),(int) (r.getHeight())));
 		
 		/* If we have nothing to visualize, then move on */
 		if (arr == null) {
 			return;
 		}
-		//WORKS BETTER WHEN THESE ARE INTS
+		
 		double eachCellXWidth =  (r.getWidth()/arr[0].length);
 		double eachCellYWidth =  (r.getHeight()/arr.length);
-		System.out.println("Widget will use x width: " + eachCellXWidth * arr[0].length);
-		System.out.println("Widget will use y height: " + eachCellYWidth * arr.length);
-		System.out.println("Widget has potential to use width: " + r.getWidth() + ", height: " + r.getHeight());
+		//System.out.println("Widget will use x width: " + eachCellXWidth * arr[0].length);
+		//System.out.println("Widget will use y height: " + eachCellYWidth * arr.length);
+		//System.out.println("Widget has potential to use width: " + r.getWidth() + ", height: " + r.getHeight());
 		g2d = (Graphics2D) g.create();
 		g2d.setColor(Color.BLACK);
 		g2d.setStroke(new BasicStroke(1));
 		
 		for (int y=0; y<arr.length; y++) {
 			for (int x=0; x<arr[0].length; x++) {
-				//Draw the rectangles when we need to
-				if (arr[0].length < 350 && arr.length < 350) {
-					g2d.drawRect((int)(x*eachCellXWidth), (int)(y*eachCellYWidth), (int)(eachCellXWidth), (int)(eachCellYWidth));
-				}
+				//Always draw rectangle
+				g2d.setColor(Color.LIGHT_GRAY);
+				g2d.drawRect((int)(x*eachCellXWidth), (int)(y*eachCellYWidth), (int)(eachCellXWidth), (int)(eachCellYWidth));
 				//Draw oval when needed
 				if (arr[y][x]) {
+					g2d.setColor(Color.BLACK);
 					g2d.fillOval((int)(x*eachCellXWidth), (int)(y*eachCellYWidth), (int)(eachCellXWidth), (int)(eachCellYWidth));
 
 				}
@@ -79,9 +77,8 @@ public class GridVisualizerWidget extends JPanel implements MouseListener {
 	public void mouseClicked(MouseEvent e) {
 		Dimension temp = convertPointToCoordinate(e.getX(), e.getY());
 		if (temp.width < 0 || temp.height < 0 || temp.width >= arr[0].length || temp.height >= arr.length)
-			return; //Temp solution
+			return; 
 		notifyObservers("button_clicked", temp.width, temp.height);
-		//TODO dont use dimensions make a new class
 	}
 
 	@Override
@@ -95,16 +92,7 @@ public class GridVisualizerWidget extends JPanel implements MouseListener {
 	
 	@Override
 	public void mouseEntered(MouseEvent e) {
-		//System.out.println("mouse entered" + e.getX());
-		//Dimension temp = convertPointToCoordinate(e.getX(), e.getY());
-		//Rectangle r = getBounds();
-		//int eachCellXWidth = (int) (r.getWidth()/arr[0].length);
-		//int eachCellYWidth = (int) (r.getHeight()/arr.length);
-		//g2d.setColor(Color.YELLOW);
-		//g2d.fillRect((int) (temp.getWidth()*eachCellXWidth+30), (int) (temp.getHeight()*eachCellYWidth), eachCellXWidth, eachCellYWidth);
-		//repaint();
 		//TODO needs to highlight spot
-		
 	}
 
 	public void addObserver(GridVisualizerWidgetObserver o) { observers.add(o); }
